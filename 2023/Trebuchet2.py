cf9a7935f1a28e34e75c439b3fc9d01ec95526bc
Porter Smith <portersmith34@gmail.com>
2024-02-21 00:00:10 -0700

    Day1 complete

---
+++ 2023/Trebuchet2.py
@@ -0,0 +1,62 @@
+import re 
+
+def intFromString(num):
+  match num:
+    case '':
+      return ''
+    case "one":
+      return 1
+    case "two":
+      return 2
+    case "three":
+      return 3
+    case "four":
+      return 4
+    case "five":
+      return 5
+    case "six":
+      return 6
+    case "seven":
+      return 7
+    case "eight":
+      return 8
+    case "nine":
+      return 9
+    case _:
+      return ''
+    
+def reverse(last):
+  temp = ""
+  for i in range(len(last)):
+    temp += last[len(last)-i-1]
+  return temp
+
+sum = 0
+file = open("calibration values.txt", "r")
+linesList= file.readlines()
+for line in linesList:
+  line = line.strip()
+  blocks = re.split("[1-9]", line)
+  nums = re.sub("[a-z]", "", line)
+  first = blocks[0]
+  last = blocks[len(blocks)-1]
+  last = reverse(last)
+  search1 = re.findall("one|two|three|four|five|six|seven|eight|nine", first)
+  search2 = re.findall("eno|owt|eerht|ruof|evif|xis|neves|thgie|enin", last)
+  if (len(search1) > 0):
+   first = intFromString(search1[0])
+  else:
+    first = ""
+  if (len(search2) > 0):
+   temp = reverse(search2[0])
+   last = intFromString(temp)
+  else: 
+    last = ""
+
+  if (first == ''):
+    first = nums[0]
+  if (last == ''):
+    last = nums[len(nums)-1]
+  val = str(first) + str(last)
+  sum += int(val)
+print(sum)
