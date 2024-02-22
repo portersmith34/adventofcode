6c053b9a55839c6ecc3fcccd62baa59a1eb484d0
Porter Smith <portersmith34@gmail.com>
2024-02-21 23:04:41 -0700

    passed part 1

--- 2023/day2/cubes.py
+++ 2023/day2/cubes.py
@@ -1,9 +1,31 @@
+import re
 numRed = 12
 numGreen = 13
 numBlue = 14
+count = 0
+numPattern = r"[0-9]+"
 
 file = open("cubegame input.txt", "r")
 linesList= file.readlines()
 
-for line in linesList[:10]:
-  print(line)
+for line in linesList:
+  clear = True
+  split = line.split(":")
+  id = re.search(numPattern, split[0]).group()
+  sets = split[1].split(";")
+  for set in sets:
+    red = re.findall(r"[0-9]+\sred", set)
+    green = re.findall(r"[0-9]+\sgreen", set)
+    blue = re.findall(r"[0-9]+\sblue", set)
+    if (len(red) > 0 and int(re.findall(numPattern, red[0])[0]) > numRed):
+      clear = False
+      break
+    if (len(green) > 0 and int(re.findall(numPattern, green[0])[0]) > numGreen):
+      clear = False
+      break
+    if (len(blue) > 0 and int(re.findall(numPattern, blue[0])[0]) > numBlue):
+      clear = False
+      break
+  if (clear):
+    count += int(id)
+print(count)
