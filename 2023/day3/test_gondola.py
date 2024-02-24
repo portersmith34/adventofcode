3238644cf260044921375419d43c8fe0c3bf2b21
Porter Smith <portersmith34@gmail.com>
2024-02-23 17:16:59 -0700

    Update test_gondola.py

--- 2023/day3/test_gondola.py
+++ 2023/day3/test_gondola.py
@@ -9,7 +9,3 @@ class TestGondola(unittest.TestCase):
     def test_input(self):
         sum = gondola.find_part_numbers(self.lines)
         self.assertEqual(sum, 4361)
-
-
-if __name__ == "__main__":
-    unittest.main()
